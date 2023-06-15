f22f95c1bd043d852cc9889623b57bd5e34ab39e
FanaticExplorer <andrey.gurtov@gmail.com>
2023-06-15 19:11:09 +0300

    Changed logic of getting all env variables

--- config_reader.py
+++ config_reader.py
@@ -1,32 +1,12 @@
-from pydantic import BaseSettings, SecretStr, validator
+from pydantic import BaseSettings, SecretStr
 import os
 
 class Settings(BaseSettings):
-    # Желательно вместо str использовать SecretStr 
-    # для конфиденциальных данных, например, токена бота
     bot_token: SecretStr
 
-    # Вложенный класс с дополнительными указаниями для настроек
     class Config:
-        # Имя файла, откуда будут прочитаны данные 
-        # (относительно текущей рабочей директории)
-        env_file = '.env'
-        # Кодировка читаемого файла
+        env_file = os.getenv('PATH_ENV') if os.getenv('LOCATION') != 'DEV' else '.env'
         env_file_encoding = 'utf-8'
 
-    # Define a validator function for the bot_token field
-    @validator("bot_token", pre=True)
-    def get_bot_token(cls, value):
-        # If the value is not provided, try to get it from the environment variable BOT_TOKEN
-        if value is None:
-            print(".env not found, using environment variable instead")
-            value = os.environ.get("BOT_TOKEN")
-        # Return the value or raise an exception if it is still None
-        if value is None:
-            raise ValueError("No bot token provided")
-        return value
-
-# При импорте файла сразу создастся 
-# и провалидируется объект конфига, 
-# который можно далее импортировать из разных мест
 config = Settings()
+
